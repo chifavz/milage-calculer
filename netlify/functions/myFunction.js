f32213a0f9c6417ea4e13bd03136e4d4490d297d
chifavz <chifavz@gmail.com>
2024-01-05 22:03:08 -0500

    modified function

--- netlify/functions/myFunction.js
+++ netlify/functions/myFunction.js
@@ -1,13 +1,14 @@
 // netlify/functions/myFunction.js
 
-export async function handler() {
+exports.handler = async (event, context) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "Hello from myFunction!" }),
       headers: {
         "Access-Control-Allow-Origin": "*", // Allow requests from any origin
         "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE", // Specify the allowed HTTP methods
       },
     };
-  }
+  };
   
